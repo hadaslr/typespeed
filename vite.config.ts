a396b839b452014d4db1b3a599aa125b295824a7
Hadas Lev Ron <hadasnoy10@gmail.com>
2025-03-23 22:37:33 +0200

    Your commit message here

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
  
   plugins: [react(),
     tailwindcss(),
   ],
 
   base: '/typespeed/',
-=======
-  base: "/typespeed/",
-  plugins: [react(),
-    tailwindcss(),
-  ],
->>>>>>> 7a93d5b2cb6df4dd08887471a7f45cb59a6445c0
  
 })
